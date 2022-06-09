470a829be63c25eae97e8419934e4e8056168595
yingyi <jinzhongjia@qq.com>
2022-06-09 20:35:45 +0800

    完成一部分

--- src/components/User/panel/PanelMovies/PanelMovies.vue
+++ src/components/User/panel/PanelMovies/PanelMovies.vue
@@ -1,29 +1,58 @@
 <script setup lang="ts">
 	import { Ref } from "vue";
 	import type { DataTableColumns } from "naive-ui";
+	import { Key } from "@vicons/ionicons5";
 	interface movie {
 		id: number;
 		name: string;
 		description: string;
+		duration: string;
+		sourceID: number;
+		classID: number;
 	}
 	const columns = ref<DataTableColumns<movie>>([
 		{
 			title: "ID",
 			key: "id",
+			width: "100px",
+			align: "center",
 		},
 		{
 			title: "影片名",
 			key: "name",
+			width: "250px",
 			ellipsis: true,
+			align: "center",
+		},
+		{
+			title: "时长",
+			key: "duration",
+			width: "100px",
+			align: "center",
 		},
 		{
 			title: "简介",
 			key: "description",
 			ellipsis: true,
+			align: "center",
+		},
+		{
+			title: "资源库ID",
+			key: "sourceID",
+			align: "center",
+			width: "100px",
+		},
+		{
+			title: "采集类ID",
+			key: "classID",
+			align: "center",
+			width: "100px",
 		},
 		{
 			title: "操作",
 			key: "action",
+			width: "150px",
+			align: "center",
 		},
 	]);
 
@@ -35,7 +64,7 @@
 </script>
 
 <template>
-	<n-data-table :columns="columns" :data="data" :bordered="false" />
+	<n-data-table :columns="columns" :data="data" :bordered="false" :single-line="false" />
 </template>
 
 <style></style>
