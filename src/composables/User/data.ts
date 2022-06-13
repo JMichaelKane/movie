b5a3842450d8d20cea85a79917e42f8e930a9847
yingyi <jinzhongjia@qq.com>
2022-06-13 11:12:25 +0800

    增加一个控制

--- src/composables/User/data.ts
+++ src/composables/User/data.ts
@@ -2,6 +2,8 @@ import { ref } from "vue";
 
 import { Category, Source, Movie } from "./public";
 
+const LogInEd = ref<boolean>(false);
+
 const sources = ref<Source[]>([]); // 采集源
 
 const categories = ref<Category[]>([]); // 自建分类
@@ -10,4 +12,4 @@ const movies = ref<Movie[]>([]); //影片
 
 const moviesNum = ref<number>(0);
 
-export { sources, moviesNum, categories, movies };
+export { sources, moviesNum, categories, movies, LogInEd };
