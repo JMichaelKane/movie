10ab3d3dfa3deb0ae04dcf081fc2bca7b80dcb08
jinzhongjia <jinzhongjia@qq.com>
2022-07-09 15:11:42 +0800

    剔除后台资源

--- src/router/router.ts
+++ src/router/router.ts
@@ -23,60 +23,6 @@ const routes: Array<RouteRecordRaw> = [
 			},
 		],
 	},
-	{
-		path: "/user",
-		name: "user",
-		component: () => import("../components/User.vue"),
-		children: [
-			{
-				path: "panel",
-				name: "panel",
-				component: () => import("../components/User/panel/PanelHome/PanelHome.vue"),
-			},
-			{
-				path: "source",
-				name: "source",
-				component: () => import("../components/User/panel/PanelSource/PanelSource.vue"),
-			},
-			{
-				path: "category",
-				name: "category",
-				component: () => import("../components/User/panel/PanelCategory/PanelCategory.vue"),
-			},
-			{
-				path: "movies",
-				name: "movies",
-				component: () => import("../components/User/panel/PanelMovies/PanelMovies.vue"),
-			},
-			{
-				path: "class",
-				name: "class",
-				component: () => import("../components/User/panel/PanelClass/PanelClass.vue"),
-			},
-			{
-				path: "setting",
-				name: "setting",
-				component: () => import("../components/User/Setting/Setting.vue"),
-			},
-			{
-				path: "source/:id/movies",
-				name: "source-detail-movie",
-				component: () =>
-					import("../components/User/panel/PanelSourceMovie/PanelSourceMovie.vue"),
-			},
-			{
-				path: "source/:id/class",
-				name: "source-detail-class",
-				component: () =>
-					import("../components/User/panel/PanelSourceClass/PanelSourceClass.vue"),
-			},
-		],
-	},
-	{
-		path: "/user/login",
-		name: "login",
-		component: () => import("../components/Login.vue"),
-	},
 ];
 
 const router = createRouter({
